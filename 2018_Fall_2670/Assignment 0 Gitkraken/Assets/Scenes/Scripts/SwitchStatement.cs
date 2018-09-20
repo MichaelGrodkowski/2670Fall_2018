5c6a0d53db9e1dfb016cff1f34c0a8be6eceb244
Unknown <michael_grodkowski@yahoo.com>
2018-09-20 17:15:11 -0600

    Trying to figure out switch statements to make random damage.

--- 2018_Fall_2670/Assignment 0 Gitkraken/Assets/Scenes/Scripts/SwitchStatement.cs
+++ 2018_Fall_2670/Assignment 0 Gitkraken/Assets/Scenes/Scripts/SwitchStatement.cs
@@ -1,13 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 //public class SwitchStatement : MonoBehaviour {
-//Random DamageGenerator = new Random();
-   
-//    public Random DamageGenerator1
-  //  {
-    //    get { return DamageGenerator; }
+//public int PlayerHealth == 100;
+//    Random DamageGenerator = new Random();
+
+//    Random int (1, 10);
+//    Random int (1, 101);
+    
+//    void Start()
+//    {
+//        print(PlayerHealth);
+//    }
+
+//    private void OnCollisionEnter;
+
+//    private SwitchStatement DamageGenerator;
+ //   {
+  //      case 3;
+  //          Console.WriteLine("Taken Damage!");
+  //      case 2;
+  //          Console.WriteLine("We can't take much more captain!");
+  //      case 1; 
+  //          Console.WriteLine("we're going down! Abandon ship!");
+//    }
+        
+//   public Random DamageGenerator1
+//     get { return DamageGenerator; }
       //  set { DamageGenerator = value; }
         
    // }
