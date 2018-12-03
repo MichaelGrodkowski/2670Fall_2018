02826a0de8ca712a38d66ccbb8330fb07e715c8e
Unknown <michael_grodkowski@yahoo.com>
2018-12-02 20:12:02 -0700

    Beginning on scriptable object obstacles

---
+++ 2018_Fall_2670/2D Infinite Runner/Infinite Runner/Assets/Scripts/Scriptable_Obj_obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptable_Obj_obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
