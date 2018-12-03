cdddcce9c5a23cc8b59835a7202dbed8ca10a740
Unknown <michael_grodkowski@yahoo.com>
2018-12-02 21:44:14 -0700

    Created pre-fab coins

---
+++ 2018_Fall_2670/2D Infinite Runner/Infinite Runner/Assets/Scripts/Display_SO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Display_SO : MonoBehaviour
+{
+
+
+	public object obstacle;
+
+	void Start()
+	{
+
+
+	}
+}
+	
