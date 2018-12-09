6c6adad7a21111600189ccb53b04593abd9663d2
Unknown <michael_grodkowski@yahoo.com>
2018-12-08 19:35:37 -0700

    Working on the store.

---
+++ 2018_Fall_2670/2D Infinite Runner/Infinite Runner/Assets/Scripts/CoinCollections.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntDataVar", menuName = "IntData")]
+public class CoinCollections : ScriptableObject
+{
+
+    public int Value;
+
+}
