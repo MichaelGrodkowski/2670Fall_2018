2d7e359d36f934d353455b14c8551d876d57fc27
Unknown <michael_grodkowski@yahoo.com>
2018-12-11 20:47:00 -0700

    Working on events. And gettin' pissed at the store.

--- 2018_Fall_2670/2D Infinite Runner/Infinite Runner/Assets/Scripts/Enviornment/GameOverScript.cs
+++ 2018_Fall_2670/2D Infinite Runner/Infinite Runner/Assets/Scripts/Enviornment/GameOverScript.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameOverScript : MonoBehaviour {
-
-int score = 0;
+public class GameOverScript : MonoBehaviour
+{
+    private int score = 0;
 
     void Start()
     {
@@ -16,7 +16,7 @@ int score = 0;
         GUI.Label(new Rect(Screen.width / 2 - 40, 50, 80, 30), "GAME OVER");
         
         GUI.Label(new Rect(Screen.width / 2 - 40, 300, 80, 30), "Score:" + score);
-        if (GUI.Button(new Rect(Screen.width / 2 - 30, 350, 60, 30), "Play Again?"))
+        if (GUI.Button(new Rect(Screen.width / 2 - 30, 350, 60, 30), "Again?"))
         {
             Application.LoadLevel(0);
         }
